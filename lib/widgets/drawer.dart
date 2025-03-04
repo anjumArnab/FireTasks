c26ab83960a32a5b7887ea47121a819837c472f8
anjumArnab <anjumarnab050@gmail.com>
2025-03-04 22:12:13 +0600

    backup button removed

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -4,8 +4,6 @@ class CustomDrawer extends StatelessWidget {
   final String username;
   final String email;
   final String profilePictureUrl;
-  final bool isBackupEnabled;
-  final ValueChanged<bool> onBackupToggle;
   final VoidCallback onLogout;
   final VoidCallback onExit;
 
@@ -14,8 +12,6 @@ class CustomDrawer extends StatelessWidget {
     required this.username,
     required this.email,
     required this.profilePictureUrl,
-    required this.isBackupEnabled,
-    required this.onBackupToggle,
     required this.onLogout,
     required this.onExit,
   });
@@ -25,43 +21,27 @@ class CustomDrawer extends StatelessWidget {
     return Drawer(
       child: Column(
         children: [
-          // Header Section with User Profile
           UserAccountsDrawerHeader(
             accountName: Text(username),
             accountEmail: Text(email),
             currentAccountPicture: CircleAvatar(
               backgroundImage: NetworkImage(
-                  profilePictureUrl), // Replace with user's profile picture URL
+                  profilePictureUrl),
             ),
             decoration: const BoxDecoration(
-              color: Colors.black26// Set background color
+              color: Colors.black26
             ),
           ),
-          // Backup Tasks Section
-          ListTile(
-            leading: const Icon(Icons.cloud, color: Colors.black),
-            title: const Text('Backup to Cloud'), // Backup Tasks to Cloud
-            trailing: Switch(
-              focusColor: Colors.black,
-              value: isBackupEnabled,
-              onChanged: onBackupToggle,
-            ),
-          ),
-
           const Divider(),
-
-          // Logout Button
           ListTile(
             leading: const Icon(Icons.logout, color: Colors.black),
             title: const Text('Logout'),
-            onTap: onLogout, // Add logout functionality here
+            onTap: onLogout,
           ),
-
-          // Exit Button
           ListTile(
             leading: const Icon(Icons.exit_to_app, color: Colors.black),
             title: const Text('Exit'),
-            onTap: onExit, // Add exit functionality here
+            onTap: onExit,
           ),
         ],
       ),
