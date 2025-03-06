07f1c1a0a169b024fdb003cfdab4f54c2a7d7f7b
anjumArnab <anjumarnab050@gmail.com>
2025-03-06 13:17:47 +0600

    google sign in implemented

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -1,6 +1,7 @@
 import 'package:firetasks/utils/snack_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseAuthMethods {
   final FirebaseAuth _auth;
@@ -56,6 +57,28 @@ class FirebaseAuthMethods {
     try {
       await _auth.currentUser!.sendEmailVerification();
       showSnackBar(context, 'Verification email sent');
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!);
+    }
+  }
+
+  // Google Sign In
+  Future<void> signInWithGoogle(BuildContext context) async {
+    try{
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+      
+      if (googleAuth?.accessToken != null && googleAuth?.idToken != null){
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth?.accessToken,
+          idToken: googleAuth?.idToken,
+        );
+        UserCredential userCredential = await _auth.signInWithCredential(credential);
+        User? user = userCredential.user;
+        if (user != null){
+          showSnackBar(context, "Login successful!");
+      }
+      }
     } on FirebaseAuthException catch (e) {
       showSnackBar(context, e.message!);
     }
