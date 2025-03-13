9359e909fa1c9982807fd9ab41caff2a9e244d5f
anjumArnab <anjumarnab050@gmail.com>
2025-03-13 21:25:36 +0600

    ChangeNotifierProvider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firetasks/firebase_options.dart';
-import 'package:flutter/material.dart';
+import 'package:firetasks/providers/auth_provider.dart';
+import 'package:firetasks/providers/task_provider.dart';
+import 'package:firetasks/providers/user_provider.dart';
 import 'package:firetasks/screens/homepage.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,13 +21,20 @@ class FireTasks extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FireTasks',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
+      ],
+      child: MaterialApp(
+        title: 'FireTasks',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme()
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
